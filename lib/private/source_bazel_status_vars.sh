a27f14f37d5f14ccca8498a4facb359efcd894c8
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-08 14:22:18 -0700

    Started implementing generator.

---
+++ lib/private/source_bazel_status_vars.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Parses the specified status file (e.g. stable-status.txt, volatile-status.txt)
+# and makes the key-value pairs available as Bash variables. The Bash variable
+# name is of the format "WKSP__<key>".
+source_bazel_status_vars() {
+  local status_path="${1}"
+  local export_vars="${2:-FALSE}"
+
+  # Read line-by-line
+  while IFS= read -r line; do
+    # Break the line string into parts
+    read -a line_parts <<< ${line}
+
+    # The first part is the key. The remainder of the string is the value.
+    key="${line_parts[0]}"
+    if [[ ${#line_parts[@]} == 1 ]]; then
+      value=""
+    else
+      value_offset=( ${#key} + 1 )
+      value="${line:${value_offset}}"
+      # Trim leading whitespace
+      value="${value#"${value%%[![:space:]]*}"}"
+      # Trim trailing whitespace
+      value="${value%"${value##*[![:space:]]}"}"
+    fi
+    export_key="WKSP__${key}"
+    printf -v "${export_key}" %s "${value}"
+    if [[ "${export_vars}" == TRUE ]]; then
+      export "${export_key}"
+    fi
+  done < "${stable_status_path}"
+}
---
+++ rules/private/generate_workspace_snippet.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# MARK - Locate Depedencies
+
+fail_sh_location=cgrindel_bazel_starlib/lib/private/fail.sh
+fail_sh="$(rlocation "${fail_sh_location}")" || \
+  (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
+source "${fail_sh}"
+
+# MARK - Functions
+
+
+# MARK - Process Args
+
+# status_path="${script_dir}/../bazel-out"
+# stable_status_path="${status_path}/stable-status.txt"
+# volatile_status_path="${status_path}/volatile-status.txt"
+
+# source_bazel_status_vars "${stable_status_path}"
+# source_bazel_status_vars "${volatile_status_path}"
+
+status_file_paths=()
+while (("$#")); do
+  case "${1}" in
+    "--status_file")
+      status_file_paths+=( "${2}" )
+      shift 2
+      ;;
+    "--output")
+      output_path="${2}"
+      shift 2
+      ;;
+    *)
+      shift 1
+      ;;
+  esac
+done
+
+
+[[ -z "${output_path}" ]] && fail "Expected an output path."
+
+# Source the stable-status.txt and volatile-status.txt values as Bash variables
+for status_file_path in "${status_file_paths[@]:-}" ; do
+  source_bazel_status_vars "${status_file_path}"
+done
+
+
