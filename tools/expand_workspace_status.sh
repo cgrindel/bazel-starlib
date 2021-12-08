cd6ebc919785133b25be33e79be6911943fde0bf
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-08 11:38:17 -0700

    Seems to work.

--- tools/expand_workspace_status.sh
+++ tools/expand_workspace_status.sh
@@ -22,13 +22,30 @@ done
 # Read line-by-line
 lines=()
 while IFS= read -r line; do
-  lines+=( "${line}" )
+  # Break the line string into parts
+  read -a line_parts <<< ${line}
+
+  # The first part is the key. The remainder of the string is the value.
+  key="${line_parts[0]}"
+  if [[ ${#line_parts[@]} == 1 ]]; then
+    value=""
+  else
+    value_offset=( ${#key} + 1 )
+    value="${line:${value_offset}}"
+    # Trim leading whitespace
+    value="${value#"${value%%[![:space:]]*}"}"
+    # Trim trailing whitespace
+    value="${value%"${value##*[![:space:]]}"}"
+  fi
+  # DEBUG BEGIN
+  echo >&2 "*** CHUCK  key: ${key}" 
+  echo >&2 "*** CHUCK  value: ${value}" 
+  # DEBUG END
+  export_key="WKSP__${key}"
+  printf -v "${export_key}" %s "${value}"
+  # DEBUG BEGIN
+  echo >&2 "*** CHUCK  export_key: ${export_key}" 
+  echo >&2 "*** CHUCK  !export_key: ${!export_key}" 
+  # DEBUG END
 done < "${stable_status_path}"
 
-
-# DEBUG BEGIN
-echo >&2 "*** CHUCK  lines:"
-for (( i = 0; i < ${#lines[@]}; i++ )); do
-  echo >&2 "*** CHUCK   ${i}: ${lines[${i}]}"
-done
-# DEBUG END
