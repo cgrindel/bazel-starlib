bf906b3884c416e696add83304db2ac35a6dc3b6
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-08 17:53:37 -0700

    Renamed output to output_path

--- tests/rules_tests/workspace_snippet_tests/generate_workspace_snippet_test.sh
+++ tests/rules_tests/workspace_snippet_tests/generate_workspace_snippet_test.sh
@@ -11,6 +11,8 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
 
+# MARK - Locate Resources
+
 fail_sh_location=cgrindel_bazel_starlib/lib/private/fail.sh
 fail_sh="$(rlocation "${fail_sh_location}")" || \
   (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
@@ -19,6 +21,8 @@ source "${fail_sh}"
 generate_workspace_snippet_sh_location=cgrindel_bazel_starlib/tools/generate_workspace_snippet.sh
 generate_workspace_snippet_sh="$(rlocation "${generate_workspace_snippet_sh_location}")" || \
   (echo >&2 "Failed to locate ${generate_workspace_snippet_sh_location}" && exit 1)
+
+# MARK - Set up Status Files
 
 stable_status_path="stable-status.txt"
 cat > "${stable_status_path}" <<-EOF
@@ -33,7 +37,9 @@ cat > "${volatile_status_path}" <<-EOF
 VOLATILE_VALUE smidgen
 EOF
 
-output="actual_snippet.bzl"
+# MARK - Test Successful Snippet Generation
+
+output_path="actual_snippet.bzl"
 workspace_name="acme_rules_fun"
 url1='http://github.com/acme/rules_fun/releases/download/${STABLE_CURRENT_RELEASE_TAG}/rules_fun-${STABLE_CURRENT_RELEASE}.tar.gz'
 url2='http://mirror.bazel.build/github.com/acme/rules_fun/releases/download/${STABLE_CURRENT_RELEASE_TAG}/rules_fun-${STABLE_CURRENT_RELEASE}.tar.gz'
@@ -42,14 +48,14 @@ sha256=5b80d60e00a7ea2d9d540c594e5ec41c946c163e272056c626026fcbb7918de2
 "${generate_workspace_snippet_sh}" \
   --status_file "${stable_status_path}" \
   --status_file "${volatile_status_path}" \
-  --output "${output}" \
+  --output "${output_path}" \
   --workspace_name "${workspace_name}" \
   --url "${url1}" \
   --url "${url2}" \
   --sha256 "${sha256}"
 
-[[ -f "${output}" ]] || fail "Expected output file to exist. ${output}"
-actual_snippet="$(< "${output}")"
+[[ -f "${output_path}" ]] || fail "Expected output file to exist. ${output_path}"
+actual_snippet="$(< "${output_path}")"
 expected_snippet=$(cat <<-EOF
 http_archive(
     name = "acme_rules_fun",
@@ -64,3 +70,4 @@ EOF
 [[ "${actual_snippet}" == "${expected_snippet}" ]] || \
   fail $'Snippet did not match expected.  actual:\n'"${actual_snippet}"$'\nexpected:\n'"${expected_snippet}"
 
+# MARK - Test Missing Output
