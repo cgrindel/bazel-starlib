52aa0e877386500ccc1b8918bb43d44388028389
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-13 16:20:59 -0700

    Added get_gh_repo_name_test.

--- lib/private/github.sh
+++ lib/private/github.sh
@@ -5,6 +5,8 @@
 # This is used to determine if the library has been loaded
 cgrindel_bazel_starlib_lib_private_github_loaded() { return; }
 
+# MARK - Github Auth Status Functions
+
 # Returns the raw gh auth status displyaing the auth token.
 get_gh_auth_status() {
   gh auth status -t
@@ -33,18 +35,20 @@ get_gh_auth_token() {
     sed -E -n 's/^.* Token:[[:space:]]+([^[:space:]]+).*/\1/gp'
 }
 
+# MARK - Github Repo URL Functions
+
 # List of valid Github URL patterns
 _github_url_patterns=()
 _github_url_owner_sed_cmds=()
 
-
-# git@github.com:cgrindel/bazel-starlib.git
+# Example: git@github.com:cgrindel/bazel-starlib.git
 _github_url_patterns+=(git@github.com:)
 _github_url_owner_sed_cmds+=('s|git@[^:]+:([^/]+).*|\1|gp')
 
-# https://github.com/cgrindel/bazel-starlib.git
+# Example: https://github.com/cgrindel/bazel-starlib.git
 _github_url_patterns+=(https://github.com)
 _github_url_owner_sed_cmds+=('s|https://github.com/([^/]+)/.*|\1|gp')
+
 
 # Returns the index for the Github URL pattern. This index can be used look up
 # owner and repo name.
@@ -69,4 +73,10 @@ get_gh_repo_owner() {
   local pattern_index=$( _get_github_repo_pattern_index "${repo_url}" )
   local sed_cmd="${_github_url_owner_sed_cmds[${pattern_index}]}"
   echo "${repo_url}" | sed -E -n "${sed_cmd}"
+}
+
+# Return the Github repository name from the repository URL.
+get_gh_repo_name() {
+  local repo_url="${1}"
+  basename -s .git "${repo_url}"
 }
