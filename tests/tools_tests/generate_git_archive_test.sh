1bd7728f370a70c2002b9b7fa5b2fc028bd6f41b
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-15 14:20:59 -0700

    Added placeholder for generate_git_archive_test.

---
+++ tests/tools_tests/generate_git_archive_test.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# MARK - Dependencies
+
+fail_sh_location=cgrindel_bazel_starlib/lib/private/fail.sh
+fail_sh="$(rlocation "${fail_sh_location}")" || \
+  (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
+source "${fail_sh}"
+
+env_sh_location=cgrindel_bazel_starlib/lib/private/env.sh
+env_sh="$(rlocation "${env_sh_location}")" || \
+  (echo >&2 "Failed to locate ${env_sh_location}" && exit 1)
+source "${env_sh}"
+
+setup_git_repo_sh_location=cgrindel_bazel_starlib/tests/setup_git_repo.sh
+setup_git_repo_sh="$(rlocation "${setup_git_repo_sh_location}")" || \
+  (echo >&2 "Failed to locate ${setup_git_repo_sh_location}" && exit 1)
+
+generate_git_archive_sh_location=cgrindel_bazel_starlib/tools/generate_git_archive.sh
+generate_git_archive_sh="$(rlocation "${generate_git_archive_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_git_archive_sh_location}" && exit 1)
+
+is_installed git || fail "Could not find git."
+
+# MARK - Setup
+
+source "${setup_git_repo_sh}"
+cd "${repo_dir}"
+
+
+# MARK - Test
+
+fail "IMPLEMENT ME!"
