ca4619c38d574b7e2deeb73140e2aa0321024b41
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-15 17:33:28 -0700

    Save work.

--- tools/generate_release_notes.sh
+++ tools/generate_release_notes.sh
@@ -22,6 +22,18 @@ generate_gh_changelog_sh_location=cgrindel_bazel_starlib/tools/generate_gh_chang
 generate_gh_changelog_sh="$(rlocation "${generate_gh_changelog_sh_location}")" || \
   (echo >&2 "Failed to locate ${generate_gh_changelog_sh_location}" && exit 1)
 
+generate_git_archive_sh_location=cgrindel_bazel_starlib/tools/generate_git_archive.sh
+generate_git_archive_sh="$(rlocation "${generate_git_archive_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_git_archive_sh_location}" && exit 1)
+
+generate_sha256_sh_location=cgrindel_bazel_starlib/tools/generate_sha256.sh
+generate_sha256_sh="$(rlocation "${generate_sha256_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_sha256_sh_location}" && exit 1)
+
+generate_workspace_snippet_sh_location=cgrindel_bazel_starlib/tools/generate_workspace_snippet.sh
+generate_workspace_snippet_sh="$(rlocation "${generate_workspace_snippet_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_workspace_snippet_sh_location}" && exit 1)
+
 # MARK - Process Arguments
 
 remote_name=origin
@@ -30,10 +42,10 @@ main_branch=main
 args=()
 while (("$#")); do
   case "${1}" in
-    "--previous_tag_name")
-      previous_tag_name="${2}"
-      shift 2
-      ;;
+    # "--previous_tag_name")
+    #   previous_tag_name="${2}"
+    #   shift 2
+    #   ;;
     "--output")
       output_path="${2}"
       shift 2
@@ -53,10 +65,24 @@ tag_name="${args[0]}"
 
 cd "${BUILD_WORKSPACE_DIRECTORY}"
 
+# TODO: Add warning if this is being run on MacOS about SHA256 for archive not
+# being the same as Github archive.
+
 changelog_md="$( "${generate_gh_changelog_sh}" "${tag_name}" )"
+archive_sha256="$( "${generate_git_archive_sh}" --tag_name "${tag_name}" | "${generate_sha256_sh}")"
+# workspace_snippet="$( "${generate_workspace_snippet_sh}" )"
+workspace_snippet=""
 
 release_notes_md="$(cat <<-EOF
 ${changelog_md}
+
+## Workspace Snippet
+
+\`\`\`python
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+${workspace_snippet}
+\`\`\`
 EOF
 )"
 
