5a898bcc7f24ea9c7c05f59cb7e909d316e3b408
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-15 17:10:21 -0700

    Generating release notes with changelog.

--- tools/generate_release_notes.sh
+++ tools/generate_release_notes.sh
@@ -18,6 +18,51 @@ fail_sh="$(rlocation "${fail_sh_location}")" || \
   (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
 source "${fail_sh}"
 
-# DEBUG BEGIN
-fail "STOP"
-# DEBUG END
+generate_gh_changelog_sh_location=cgrindel_bazel_starlib/tools/generate_gh_changelog.sh
+generate_gh_changelog_sh="$(rlocation "${generate_gh_changelog_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_gh_changelog_sh_location}" && exit 1)
+
+# MARK - Process Arguments
+
+remote_name=origin
+main_branch=main
+
+args=()
+while (("$#")); do
+  case "${1}" in
+    "--previous_tag_name")
+      previous_tag_name="${2}"
+      shift 2
+      ;;
+    "--output")
+      output_path="${2}"
+      shift 2
+      ;;
+    *)
+      args+=("${1}")
+      shift 1
+      ;;
+  esac
+done
+
+[[ ${#args[@]} == 0 ]] && fail "A tag name for the release must be specified."
+tag_name="${args[0]}"
+
+
+# MARK - Generate the changelog.
+
+cd "${BUILD_WORKSPACE_DIRECTORY}"
+
+changelog_md="$( "${generate_gh_changelog_sh}" "${tag_name}" )"
+
+release_notes_md="$(cat <<-EOF
+${changelog_md}
+EOF
+)"
+
+# Output the changelog
+if [[ -z "${output_path:-}" ]]; then
+  echo "${release_notes_md}"
+else
+  echo "${release_notes_md}" > "${output_path}"
+fi
