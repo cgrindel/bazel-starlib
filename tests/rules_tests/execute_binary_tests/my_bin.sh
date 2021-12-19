463ea2ba993f731c5e0e02e93f4ed2111f033652
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-19 10:53:46 -0700

    Clean up.

--- tests/rules_tests/execute_binary_tests/my_bin.sh
+++ tests/rules_tests/execute_binary_tests/my_bin.sh
@@ -43,21 +43,10 @@ while (("$#")); do
 done
 
 if [[ "${check_data_file}" == true ]]; then
-  # tests/rules_tests/execute_binary_tests
   data_txt_location=cgrindel_bazel_starlib/tests/rules_tests/execute_binary_tests/data.txt
   data_txt="$(rlocation "${data_txt_location}")" || \
     (echo >&2 "Failed to locate ${data_txt_location}" && exit 1)
   echo "Data: $(< "${data_txt}")"
 fi
-
-# DEBUG BEGIN
-echo >&2 "*** CHUCK  input_file: ${input_file:-}" 
-tree >&2
-# input_file_location="${input_file}"
-# input_file_path="$(rlocation "${input_file_location}")" || \
-#   (echo >&2 "Failed to locate ${input_file_location}" && exit 1)
-#   # (echo >&2 "Failed to locate ${"${input_file_location}${input_file}"_location}" && exit 1)
-# echo >&2 "*** CHUCK  input_file_path: ${input_file_path}" 
-# DEBUG END
 
 [[ -z "${input_file:-}" ]] || echo "Input: $(< "${input_file}")"
