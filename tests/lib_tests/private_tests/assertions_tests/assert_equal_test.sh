bb75330e4a0215cfb3d58641026fecae87c86bc0
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-22 10:23:07 -0700

    Moved assert_fail to a sh_library.

--- tests/lib_tests/private_tests/assertions_tests/assert_equal_test.sh
+++ tests/lib_tests/private_tests/assertions_tests/assert_equal_test.sh
@@ -16,36 +16,13 @@ assertions_sh="$(rlocation "${assertions_sh_location}")" || \
   (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
 source "${assertions_sh}"
 
+assert_fail_sh_location=cgrindel_bazel_starlib/tests/lib_tests/private_tests/assertions_tests/assert_fail.sh
+assert_fail_sh="$(rlocation "${assert_fail_sh_location}")" || \
+  (echo >&2 "Failed to locate ${assert_fail_sh_location}" && exit 1)
+source "${assert_fail_sh}"
+
 # MARK - Replace fail
 
-FAIL_ERR_MSGS=()
-
-fail() {
-  local err_msg="${1:-Unspecified error occurred.}"
-  FAIL_ERR_MSGS+=( "${err_msg}" )
-}
-
-reset_fail_err_msgs() {
-  FAIL_ERR_MSGS=()
-}
-
-new_fail(){
-  local err_msg="${1:-}"
-  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
-  echo >&2 "${err_msg}"
-  exit 1
-}
-
-assert_fail() {
-  local pattern=${1}
-  [[ ${#FAIL_ERR_MSGS[@]} == 0 ]] && new_fail "Expected a failure. None found. pattern: ${pattern}"
-  [[ ${#FAIL_ERR_MSGS[@]} > 1 ]] && new_fail "Expected a single failure. Found ${#FAIL_ERR_MSGS[@]}. pattern: ${pattern}"
-  [[ "${FAIL_ERR_MSGS[0]}" =~ ${pattern} ]] || new_fail "Unexpected failure. Found ${FAIL_ERR_MSGS[0]}. pattern: ${pattern}"
-}
-
-assert_no_fail(){
-  [[ ${#FAIL_ERR_MSGS[@]} == 0 ]] || new_fail "Expected no failures. Found ${#FAIL_ERR_MSGS[@]}. ${FAIL_ERR_MSGS[@]}"
-}
 
 # MARK - Test assert_equal
 
