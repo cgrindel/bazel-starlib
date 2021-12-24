b2ca1116c9d56d010eb6808e284c258c3695b949
Chuck Grindel <chuck@chuckgrindel.com>
2021-12-24 15:41:16 -0700

    tools test is green.

--- tests/tools_tests/update_readme_test.sh
+++ tests/tools_tests/update_readme_test.sh
@@ -11,9 +11,53 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
 
+# MARK - Locate Deps
+
 assertions_sh_location=cgrindel_bazel_starlib/lib/private/assertions.sh
 assertions_sh="$(rlocation "${assertions_sh_location}")" || \
   (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
 source "${assertions_sh}"
 
-fail "IMPLEMENT ME!"
+generate_fake_snippet_sh_location=cgrindel_bazel_starlib/tests/tools_tests/generate_fake_snippet.sh
+generate_fake_snippet_sh="$(rlocation "${generate_fake_snippet_sh_location}")" || \
+  (echo >&2 "Failed to locate ${generate_fake_snippet_sh_location}" && exit 1)
+
+update_readme_sh_location=cgrindel_bazel_starlib/tools/update_readme.sh
+update_readme_sh="$(rlocation "${update_readme_sh_location}")" || \
+  (echo >&2 "Failed to locate ${update_readme_sh_location}" && exit 1)
+
+
+# MARK - Set up
+
+export BUILD_WORKSPACE_DIRECTORY="${PWD}"
+
+tag_name="v99999.0.0"
+
+readme_content="$(cat <<-EOF
+Text before snippet
+<!-- BEGIN WORKSPACE SNIPPET -->
+Text should be replaced
+<!-- END WORKSPACE SNIPPET -->
+Text after snippet
+EOF
+)"
+
+
+# MARK - Test Find README.md
+
+readme_path="${BUILD_WORKSPACE_DIRECTORY}/README.md"
+
+# Write README.md
+echo "${readme_content}" > "${readme_path}"
+
+"${update_readme_sh}" \
+  --generate_workspace_snippet "${generate_fake_snippet_sh}" \
+  "${tag_name}"
+
+actual="$(< "${readme_path}")"
+assert_match "Text before snippet" "${actual}" "Find README.md"
+assert_match "Text after snippet" "${actual}" "Find README.md"
+assert_no_match "Text should be replaced" "${actual}" "Find README.md"
+
+
+assert_match "fake snippet" "${actual}" "Find README.md"
