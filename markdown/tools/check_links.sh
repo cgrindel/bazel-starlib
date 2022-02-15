0cd17057946000c2e746da0cd4b13f34df035210
Chuck Grindel <chuck@chuckgrindel.com>
2022-02-15 11:11:05 -0700

    Added check_links and check_links_test placeholders.

---
+++ markdown/tools/check_links.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# MARK - Locate Deps
+
+fail_sh_location=cgrindel_bazel_starlib/shlib/lib/fail.sh
+fail_sh="$(rlocation "${fail_sh_location}")" || \
+  (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
+source "${fail_sh}"
+
+markdown_link_check_location=cgrindel_bazel_starlib_markdown_npm/markdown-link-check/bin/markdown-link-check
+markdown_link_check="$(rlocation "${markdown_link_check_location}")" || \
+  (echo >&2 "Failed to locate ${markdown_link_check_location}" && exit 1)
+
+# MARK - Process Args
+
+
+# MARK - Check Links
