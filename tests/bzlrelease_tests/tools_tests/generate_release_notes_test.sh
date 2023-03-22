f135f5d7d136436285fc79bcd325c2f0bb5f853a
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-22 13:19:36 -0600

    Update generate_release_notes_test.

--- tests/bzlrelease_tests/tools_tests/generate_release_notes_test.sh
+++ tests/bzlrelease_tests/tools_tests/generate_release_notes_test.sh
@@ -13,15 +13,15 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 
 # MARK - Dependencies
 
-fail_sh_location=cgrindel_bazel_starlib/shlib/lib/fail.sh
-fail_sh="$(rlocation "${fail_sh_location}")" || \
-  (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
-source "${fail_sh}"
-
 env_sh_location=cgrindel_bazel_starlib/shlib/lib/env.sh
 env_sh="$(rlocation "${env_sh_location}")" || \
   (echo >&2 "Failed to locate ${env_sh_location}" && exit 1)
 source "${env_sh}"
+
+assertions_sh_location=cgrindel_bazel_starlib/shlib/lib/assertions.sh
+assertions_sh="$(rlocation "${assertions_sh_location}")" || \
+  (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
+source "${assertions_sh}"
 
 setup_git_repo_sh_location=cgrindel_bazel_starlib/tests/setup_git_repo.sh
 setup_git_repo_sh="$(rlocation "${setup_git_repo_sh_location}")" || \
@@ -54,6 +54,6 @@ actual="$(
     --generate_workspace_snippet "${generate_workspace_snippet_sh}" \
     "${tag}" 
 )"
-[[ "${actual}" =~ "## What's Changed" ]]
-[[ "${actual}" =~ "## Workspace Snippet" ]]
-[[ "${actual}" =~ "http_archive(" ]]
+assert_match "## What's Changed" "${actual}"
+assert_match "## Workspace Snippet" "${actual}"
+assert_match "http_archive\(" "${actual}"
