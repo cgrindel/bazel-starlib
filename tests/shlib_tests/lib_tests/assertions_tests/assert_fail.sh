b8a9508dadf76f2b090c1b43285815c6736b55a6
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-18 11:26:03 -0600

    Address issues in assert_fail.sh.

--- tests/shlib_tests/lib_tests/assertions_tests/assert_fail.sh
+++ tests/shlib_tests/lib_tests/assertions_tests/assert_fail.sh
@@ -12,19 +12,21 @@ reset_fail_err_msgs() {
 }
 
 new_fail(){
-  local err_msg="${1:-}"
-  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
-  echo >&2 "${err_msg}"
+  if [[ $# -eq 0 ]]; then
+    echo >&2 "Unspecified error occurred."
+  else
+    echo >&2 "${@}"
+  fi
   exit 1
 }
 
 assert_fail() {
   local pattern=${1}
   [[ ${#FAIL_ERR_MSGS[@]} == 0 ]] && new_fail "Expected a failure. None found. pattern: ${pattern}"
-  [[ ${#FAIL_ERR_MSGS[@]} > 1 ]] && new_fail "Expected a single failure. Found ${#FAIL_ERR_MSGS[@]}. pattern: ${pattern}"
+  [[ ${#FAIL_ERR_MSGS[@]} -gt 1 ]] && new_fail "Expected a single failure. Found ${#FAIL_ERR_MSGS[@]}. pattern: ${pattern}"
   [[ "${FAIL_ERR_MSGS[0]}" =~ ${pattern} ]] || new_fail "Unexpected failure. Found '${FAIL_ERR_MSGS[0]}'. pattern: ${pattern}"
 }
 
 assert_no_fail(){
-  [[ ${#FAIL_ERR_MSGS[@]} == 0 ]] || new_fail "Expected no failures. Found ${#FAIL_ERR_MSGS[@]}. '${FAIL_ERR_MSGS[@]}'"
+  [[ ${#FAIL_ERR_MSGS[@]} == 0 ]] || new_fail "Expected no failures. Found ${#FAIL_ERR_MSGS[@]}." "${FAIL_ERR_MSGS[@]}"
 }
