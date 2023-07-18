767046dc9938f929ca88795d1756df0b199410ea
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-18 09:36:54 -0600

    Address ShellCheck issues in generate_gh_changelog.sh.

--- bzlrelease/tools/generate_gh_changelog.sh
+++ bzlrelease/tools/generate_gh_changelog.sh
@@ -16,21 +16,25 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 fail_sh_location=cgrindel_bazel_starlib/shlib/lib/fail.sh
 fail_sh="$(rlocation "${fail_sh_location}")" || \
   (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../shlib/lib/fail.sh
 source "${fail_sh}"
 
 env_sh_location=cgrindel_bazel_starlib/shlib/lib/env.sh
 env_sh="$(rlocation "${env_sh_location}")" || \
   (echo >&2 "Failed to locate ${env_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../shlib/lib/env.sh
 source "${env_sh}"
 
 git_sh_location=cgrindel_bazel_starlib/shlib/lib/git.sh
 git_sh="$(rlocation "${git_sh_location}")" || \
   (echo >&2 "Failed to locate ${git_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../shlib/lib/git.sh
 source "${git_sh}"
 
 github_sh_location=cgrindel_bazel_starlib/shlib/lib/github.sh
 github_sh="$(rlocation "${github_sh_location}")" || \
   (echo >&2 "Failed to locate ${github_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../shlib/lib/github.sh
 source "${github_sh}"
 
 
@@ -66,7 +70,6 @@ tag_name="${args[0]}"
 
 # MARK - Generate the changelog.
 
-starting_dir="${PWD}"
 cd "${BUILD_WORKSPACE_DIRECTORY}"
 
 repo_url="$( get_git_remote_url )"
@@ -74,6 +77,7 @@ is_github_repo_url "${repo_url}" || \
   fail "The git repository's remote URL does not appear to be a Github URL. ${repo_url}"
 
 # Fetch the latest from origin
+# shellcheck disable=SC2119
 fetch_latest_from_git_remote
 
 # Construct the args for generating the changelog.
