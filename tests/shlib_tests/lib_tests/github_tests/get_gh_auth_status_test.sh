34982a5d2dc254c1cc19318c0db8aea48e1953dc
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-18 11:38:59 -0600

    Address ShellCheck issues in get_gh_auth_status_test.sh.

--- tests/shlib_tests/lib_tests/github_tests/get_gh_auth_status_test.sh
+++ tests/shlib_tests/lib_tests/github_tests/get_gh_auth_status_test.sh
@@ -16,16 +16,19 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 fail_sh_location=cgrindel_bazel_starlib/shlib/lib/fail.sh
 fail_sh="$(rlocation "${fail_sh_location}")" || \
   (echo >&2 "Failed to locate ${fail_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../../../shlib/lib/fail.sh
 source "${fail_sh}"
 
 env_sh_location=cgrindel_bazel_starlib/shlib/lib/env.sh
 env_sh="$(rlocation "${env_sh_location}")" || \
   (echo >&2 "Failed to locate ${env_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../../../shlib/lib/env.sh
 source "${env_sh}"
 
 github_sh_location=cgrindel_bazel_starlib/shlib/lib/github.sh
 github_sh="$(rlocation "${github_sh_location}")" || \
   (echo >&2 "Failed to locate ${github_sh_location}" && exit 1)
+# shellcheck source=SCRIPTDIR/../../../../shlib/lib/github.sh
 source "${github_sh}"
 
 is_installed gh || fail "Could not find Github CLI (gh)."
