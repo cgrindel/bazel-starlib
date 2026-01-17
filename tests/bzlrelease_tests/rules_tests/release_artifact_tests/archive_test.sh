285ee6ab2017457aeecd275dce352384d2488802
peakschris <77508021+peakschris@users.noreply.github.com>
2026-01-17 21:39:03 +0000

    fix: windows issues (#446)
    There are multiple issues on windows:
    
    1. https://github.com/bazelbuild/bazel-skylib/pull/527
    2. https://github.com/cgrindel/bazel-starlib/issues/448
    3. https://github.com/cgrindel/bazel-starlib/issues/449
    4. https://github.com/cgrindel/bazel-starlib/issues/447
    5. https://github.com/cgrindel/bazel-starlib/issues/450
    
    Issues 2-5 above are all fixed by this PR
    
    ### Test results (windows)
    (still filling in these results)
    Before
    --enable_runfiles 332 failures
    --noenable_run
    
    With buildifier_prebuilt update only
    --enable_runfiles 116 failures
    --noenable_run 154 failures + skips
    
    With this PR only
    --enable_runfiles 34 failures
    --noenable_runfiles 122 failures + skips
    
    With this PR and https://github.com/bazelbuild/bazel-skylib/pull/527
    --enable_runfiles 0 failures
    --noenable_runfiles 85 failures
    
    ### Future activity
    Note: most of the --noenable_runfiles failures are of this form. It
    seems that bzlformat_lint_test.sh is designed to require runfiles.
    ```
    INFO: From Testing //tests/shlib_tests/lib_tests/files_tests:bzlformat_lint_test:
    ==================== Test output for //tests/shlib_tests/lib_tests/files_tests:bzlformat_lint_test:
    D:/udu/b/356umzpb/execroot/_main/bazel-out/x64_windows-fastbuild/bin/tests/shlib_tests/lib_tests/files_tests/bzlformat_lint_test.sh: line 14: tests/shlib_tests/lib_tests/files_tests/bzlformat_lint_test_BUILD.bazel.sh: No such file or directory
    tests/shlib_tests/lib_tests/files_tests/bzlformat_lint_test_BUILD.bazel.sh failed with 127.
    1 lint tests failed.
    ================================================================================
    ```
    
    Look into this:
    ```
    INFO: From Action tests/bzlrelease_tests/rules_tests/generate_workspace_snippet_tests/archive_sha256:
    which: no shasum in (/usr/bin:/usr/bin:/bin:/c/WINDOWS:/c/WINDOWS/System32:/c/WINDOWS/System32/WindowsPowerShell/v1.0)
    ```
    
    ---------
    
    Co-authored-by: Chuck Grindel <chuck.grindel@gmail.com>

--- tests/bzlrelease_tests/rules_tests/release_artifact_tests/archive_test.sh
+++ tests/bzlrelease_tests/rules_tests/release_artifact_tests/archive_test.sh
@@ -18,16 +18,29 @@ f=
 set -e
 # --- end runfiles.bash initialization v3 ---
 
+# return a unix-style path on all platforms
+# workaround for https://github.com/bazelbuild/bazel/issues/22803
+function rlocation_as_unix() {
+  path="$(rlocation ${1})"
+  case "$(uname -s)" in
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    path=${path//\\//} # backslashes to forward
+    path=/${path//:/}  # d:/ to /d/
+    ;;
+  esac
+  echo "$path"
+}
+
 # MARK - Locate Deps
 
 assertions_sh_location=cgrindel_bazel_starlib/shlib/lib/assertions.sh
-assertions_sh="$(rlocation "${assertions_sh_location}")" ||
+assertions_sh="$(rlocation_as_unix "${assertions_sh_location}")" ||
   (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
 # shellcheck source=SCRIPTDIR/../../../../shlib/lib/assertions.sh
 source "${assertions_sh}"
 
 archive_tar_gz_location=cgrindel_bazel_starlib/tests/bzlrelease_tests/rules_tests/release_artifact_tests/archive.tar.gz
-archive_tar_gz="$(rlocation "${archive_tar_gz_location}")" ||
+archive_tar_gz="$(rlocation_as_unix "${archive_tar_gz_location}")" ||
   (echo >&2 "Failed to locate ${archive_tar_gz_location}" && exit 1)
 
 tar_exe_location=cgrindel_bazel_starlib/tools/tar/tar.exe
